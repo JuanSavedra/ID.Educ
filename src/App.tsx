bc3de443ae3b1a29d874f76f6b833eda8a3fe4d6
JuanSavedra <juansavedra827@gmail.com>
2025-09-18 14:04:27 -0300

    feat navbar

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './Navbar.tsx' 
+import Dashboard from './Dashboard.tsx'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
---
+++ src/Dashboard.tsx
@@ -0,0 +1,11 @@
+function Dashboard() {
+  return (
+    <>
+      <main className="p-8">
+        <h1 className="text-3xl font-bold">Conteúdo Principal da Página</h1>
+      </main>
+    </>
+  )
+}
+
+export default Dashboard
