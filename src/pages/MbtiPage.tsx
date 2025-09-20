71a7f4edeb84dcd566081c31584b243732ce6aaa
JuanSavedra <juansavedra827@gmail.com>
2025-09-20 11:55:03 -0300

    feat OcurrencesPage

--- src/pages/MbtiPage.tsx
+++ src/pages/MbtiPage.tsx
@@ -1,13 +1,11 @@
 function MbtiPage() {
-  return (  
-    <>
-      <div className="p-8">
-        <h1 className="text-3xl font-bold text-gray-800">MBTI</h1>
-        <p className="mt-4 text-gray-600">
-          Bem-vindo á página de MBTI! Aqui você encontra tudo sobre o seu tipo de MBTI.
-        </p>
-      </div>
-    </>
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-gray-800">MBTI</h1>
+      <p className="mt-4 text-gray-600">
+        Bem-vindo á página de MBTI! Aqui você encontra tudo sobre o seu tipo de MBTI.
+      </p>
+    </div>
   )
 }
 
